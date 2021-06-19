bd9530383786fcd0574a3e45d51ad33fdc50dbfc
Alexsandr Chevtaev <alex-chevtaev@yandex.ru>
2021-06-19 19:07:58 +0300

    Настроил кодировку UTF-8 для шаблона.

--- src/main/java/ru/neutrino/ProjectConfig.java
+++ src/main/java/ru/neutrino/ProjectConfig.java
@@ -58,6 +58,9 @@ public class ProjectConfig implements WebMvcConfigurer {
     public void configureViewResolvers(ViewResolverRegistry registry) {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setForceContentType(true);
+        resolver.setContentType("text/html; charset=UTF-8");
         registry.viewResolver(resolver);
     }
 
